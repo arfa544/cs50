dac32cc5a4e7252b076fd5c2c7d777207c072e43
arfa <82232171+arfa544@users.noreply.github.com>
2022-01-20 08:06:32 +0000

    Thu, Jan 20, 2022, 8:06 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,1 +1,28 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+    // Taking input.
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n <= 0 || n >= 9);
+
+    // Printing mario.
+    for (int i = 0; i < n; i++) // For printing new line.
+    {
+        for (int k = n; k > i + 1; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < i + 1; j++) // For printing #.
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
 
