fd1f8376b8c12e2e4474cf62e521fca2d1a725f3
arfa <82232171+arfa544@users.noreply.github.com>
2022-01-20 05:57:54 +0000

    Thu, Jan 20, 2022, 5:57 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -2,17 +2,19 @@
 #include<stdio.h>
 int main(void)
 {
+    //taking input
     int n;
     do
     {
         n=get_int("Enter a number in range 1 to 8: \n");
-        for (int i=0;i<n;i++)
-        {
-            for (int j=0;j<i+1;j++)
-            {
-                printf("#");
-            }
-            printf("\n");
-        }
     }while(n<=0 || n>=9);
+    //printing mario
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<i+1;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
