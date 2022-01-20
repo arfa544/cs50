bd29bfd584a8405a70d967d044d45ce8cf88e106
arfa <82232171+arfa544@users.noreply.github.com>
2022-01-20 04:12:55 +0000

    Thu, Jan 20, 2022, 4:12 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -2,6 +2,6 @@
 #include<stdio.h>
 int main(void)
 {
-    string name=get_string("What is your name? ");
-    printf("Hello %s \n",name);
+    string name = get_string("What is your name? ");
+    printf("Hello %s \n", name);
 }
