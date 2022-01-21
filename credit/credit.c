dc62435f70b227b3c0cd54f6f39ab8f8210c2227
arfa <82232171+arfa544@users.noreply.github.com>
2022-01-21 06:02:30 +0000

    Fri, Jan 21, 2022, 6:02 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,6 +2,8 @@
 #include<stdio.h>
 int main(void)
 {
-    string name = get_string("What is your name? ");
-    printf("Hello %s \n", name);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
 }
