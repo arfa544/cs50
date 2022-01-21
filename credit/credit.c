90737adf87b9e347173c325bcf52b1b12dc41c06
arfa <82232171+arfa544@users.noreply.github.com>
2022-01-21 11:05:39 +0000

    Fri, Jan 21, 2022, 11:05 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,4 +5,11 @@ int main(void)
     // Taking input from user.
     long n = get_long("Enter credit card number: ");
     
+    int count=0;
+    while (n != 0)
+    {
+        n=n/10;
+        count++;
+    }
+
 }
