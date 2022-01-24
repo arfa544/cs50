b76723691c7520e902019332cdc0153254cabf39
arfa <82232171+arfa544@users.noreply.github.com>
2022-01-24 15:12:42 +0000

    Mon, Jan 24, 2022, 3:12 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -88,5 +88,8 @@ int convert_to_single(int num)
 
 int get_first_digits(int x, int no_of_places, int n)
 {
-    for (int i = 0; i)
+    for (int i = 0; i < x- no_of_places; i++)
+    {
+        
+    }
 }
