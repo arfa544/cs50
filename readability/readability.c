472fd6c7bd681ea6ee09ee4616875c45721060df
arfa <82232171+arfa544@users.noreply.github.com>
2022-01-27 17:36:49 +0000

    Thu, Jan 27, 2022, 5:36 PM +00:00

---
+++ readability/readability.c
@@ -0,0 +1,17 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<ctype.h>
+
+int count_letters(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    printf("%s", text);
+    int count_letters(text)
+}
+
+int count_letters(string text)
+{
+    if(text ==)
+}
