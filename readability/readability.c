13301f3a4288eb4a09bc3191a527c103279b4279
arfa <82232171+arfa544@users.noreply.github.com>
2022-02-13 11:53:37 +0000

    Sun, Feb 13, 2022, 11:53 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,7 +16,7 @@ int main(void)
 int count_letters(string text)
 {
     int letters = 0;
-    for(int i = 0, n = strlen(text); i < n; i++)
+    for(int i = 0,str[i] != '\0';  i++)
     {
          if(isalpha(text))
          {
