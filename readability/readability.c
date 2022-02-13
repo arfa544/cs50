39a2a6fbc5a9022cc264bb883129a34a26f23d57
arfa <82232171+arfa544@users.noreply.github.com>
2022-02-13 13:30:45 +0000

    Sun, Feb 13, 2022, 1:30 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,16 +5,20 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
-    int letters, words;
+    int letters, words, sentences;
     string text = get_string("Text: ");
     printf("%s\n", text);
     letters = count_letters(text);
     printf("%i\n",letters);
     words = count_words(text);
     printf("%i\n",words);
+    sentences = count_sentences(text);
+    printf("%i\n",sentences);
+
 }
 
 int count_letters(string text)
@@ -44,5 +48,20 @@ int count_words(string text)
     }
 
     return words;
+
+}
+
+int count_sentences(string text)
+{
+    int words = 0;
+    for(int i = 0; text[i] != '\0';  i++)
+    {
+         if(text[i] == '.' )
+         {
+             letters++;
+         }
+    }
+
+    return letters;
 
 }
