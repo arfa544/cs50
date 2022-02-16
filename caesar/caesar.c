40202ba7f848cf7b34a315395b9fb6cdad3d25ec
arfa <82232171+arfa544@users.noreply.github.com>
2022-02-16 15:38:36 +0000

    Wed, Feb 16, 2022, 3:38 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-//bool only_digits(string s);
+bool only_digits(string s);
 
 int main(int argc, string argv[])
 {
@@ -11,15 +11,23 @@ int main(int argc, string argv[])
         printf("%s", argv[i]);
     }
     string s;
-    if (argc == 2)
-    {
-        return 0;
-    }
-    else
+
+    // Check if exactly 1 key is passed.
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+
+    // Check if key is digit belonging to 0 and 9 inclusive.
+    if only_digits(argv[1])
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    
+
+    return 0;
 }
 
 // bool only_digits(string s)
