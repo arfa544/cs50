a401d8490d91b37571dd92700a71d5c6113757ca
arfa <82232171+arfa544@users.noreply.github.com>
2022-02-16 15:22:19 +0000

    Wed, Feb 16, 2022, 3:22 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,27 +6,30 @@
 int main(int argc, string argv[])
 {
     printf("%i", argc);
-    for (int i = 0; )
+    for(int i=0;i<argc-1;i++)
+    {
+        printf("%s", argv[i]);
+    }
     string s;
-    if (argc == 2)
-    {
-        return 0;
-    }
-    else
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
-    }
+    // if (argc == 2)
+    // {
+    //     return 0;
+    // }
+    // else
+    // {
+    //     printf("Usage: ./caesar key\n");
+    //     return 1;
+    // }
 }
 
-bool only_digits(string s)
-{
-    if (s >= 0 && s <=9)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+// bool only_digits(string s)
+// {
+//     if (s >= 0 && s <=9)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
