ef8982fb4e35a0728f1df0e81607b5d8497f849f
arfa <82232171+arfa544@users.noreply.github.com>
2022-02-20 03:38:15 +0000

    Sun, Feb 20, 2022, 3:38 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+     // Check if exactly 1 key is passed.
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 
 }
