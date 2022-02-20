0a3158f2e780da258200237ab953795ab330ddab
arfa <82232171+arfa544@users.noreply.github.com>
2022-02-20 04:36:01 +0000

    Sun, Feb 20, 2022, 4:36 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -68,7 +68,7 @@ char rotate(char c, string key)
         {
             if(isupper(key[i]))
             {
-                return(key[i] - 32);
+                return(c - 97(key[i] - 32));
             }
         }
     }
