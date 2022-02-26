8f4a80893c6354c432a62cf21f7a1156e01835f5
arfa <82232171+arfa544@users.noreply.github.com>
2022-02-26 01:18:26 +0000

    Sat, Feb 26, 2022, 1:18 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(candidates[k].name, name) == 0)
         {
-            preferences[voter][rank]++;
+            preferences[voter][rank] = k;
             printf("%s : %i\n", candidates[k].name, preferences[voter][rank]);
             return true;
         }
