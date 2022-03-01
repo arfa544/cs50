69ec7e420b5e7a08e29ab714f62966eda75a9943
arfa <82232171+arfa544@users.noreply.github.com>
2022-03-01 17:17:30 +0000

    Tue, Mar 1, 2022, 5:17 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -123,7 +123,7 @@ void record_preferences(int ranks[])
         for (int j = i + 1; j < candidate_count; j++)
         {
             preferences[ranks[i]][ranks[j]]++;
-            printf("%i\n", preferences[ranks[i]][ranks[j]]);
+            //printf("%i\n", preferences[ranks[i]][ranks[j]]);
         }
     }
     return;
