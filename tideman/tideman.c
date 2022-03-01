50a1b9f2c026302908568987f896c94cdcf3d8db
arfa <82232171+arfa544@users.noreply.github.com>
2022-03-01 15:41:56 +0000

    Tue, Mar 1, 2022, 3:41 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -205,7 +205,8 @@ bool cycle(int winner, int loser)
         {
             if (locked[i]winner)
             {
-                
+                found = true;
+                winner = i;
             }
         }
 
