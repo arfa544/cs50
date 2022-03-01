9a8aa6918d17268a5f01429d8e249c4d451e6481
arfa <82232171+arfa544@users.noreply.github.com>
2022-03-01 12:01:15 +0000

    Tue, Mar 1, 2022, 12:01 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -134,7 +134,7 @@ void add_pairs(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int i = 0; i < candidate_count; i++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (preferences[i][j] > preferences[j][i])
             {
