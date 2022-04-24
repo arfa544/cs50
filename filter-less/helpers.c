ba1a8f5c8121624777fc1d5675ed03c72e5fc4e1
arfa <82232171+arfa544@users.noreply.github.com>
2022-04-24 10:21:05 +0000

    Sun, Apr 24, 2022, 10:21 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -95,7 +95,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width / 2; j++)
+        for (int j = 0; j < width; j++)
         {
             int totalRed, totalBlue, totalGreen;
             totalRed = totalBlue = totalGreen = 0;
