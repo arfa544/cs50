f5875256cf6085a63995ce08e2f31e2800eba175
arfa <82232171+arfa544@users.noreply.github.com>
2022-05-05 14:10:16 +0000

    Thu, May 5, 2022, 2:10 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,6 +3,21 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //Each row.
+    for (int i = 0; i < height; i++)
+    {
+        //each column.
+        for (int j = 0; j < width; j++)
+        {
+            float Red = image[i][j].rgbtRed;
+            float Green = image[i][j].rgbtGreen;
+            float Blue = image[i][j].rgbtBlue;
+
+            //Find average.
+            int average = round((Red + Green + Blue) / 3);
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
