c455af57dcb35f76e81118129ca0f66dc4e7e225
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-10 17:44:18 +0000

    Fri, Jun 10, 2022, 5:44 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,13 +1,16 @@
 # TODO
 from cs50 import get_int
 
+# Taking input from user
 while (True):
     Height = get_int("Height: ")
     if (Height > 0 or Height < 9):
         break
 
-for i in range(1,Height+1):
-    print(" " * (Height - i), end = "")
-    print("#" * i, end = "")
+for i in range(1, Height+1):
+    # Printing spaces
+    print(" " * (Height - i), end="")
+    # Printing '#'
+    print("#" * i, end="")
     print()
 
