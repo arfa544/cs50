583ec6a0fcb42716b9faf226ee103529bee06a70
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-10 17:30:35 +0000

    Fri, Jun 10, 2022, 5:30 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,5 +1,7 @@
 # TODO
-Height = int(input("Height: "))
+while (True):
+    Height = int(input("Height: "))
+
 for i in range(1,Height+1):
     print(" " * (Height - i), end = "")
     print("#" * i, end = "")
