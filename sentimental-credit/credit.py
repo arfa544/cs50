a78d3eb64003dbe0746d73e0d2399429b7f86b34
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-11 03:35:30 +0000

    Sat, Jun 11, 2022, 3:35 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,7 +2,8 @@
 from cs50 import get_int
 
 # Taking input from user.
-n = get_int("Number: ")
+#n = get_int("Number: ")
 list = []
-for i in n:
-    list[i] = n[i]
+for i in list:
+    list[i] = get_int("Number: ")
+# print (list)
