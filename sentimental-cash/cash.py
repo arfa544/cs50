92537f9775f7e20d2623f5495ffe3700add4df9b
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-16 16:20:12 +0000

    Thu, Jun 16, 2022, 4:20 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -6,33 +6,31 @@ def main():
     cents = get_cents()
 
     # Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents)
+    quarters = calculate_quarters(cents)
     cents = cents - quarters * 25
 
     # Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents)
+    dimes = calculate_dimes(cents)
     cents = cents - dimes * 10
 
     # Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents)
+    nickels = calculate_nickels(cents)
     cents = cents - nickels * 5
 
     # Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents)
+    pennies = calculate_pennies(cents)
     cents = cents - pennies * 1
 
     # Sum coins
-    int coins = quarters + dimes + nickels + pennies
+    coins = quarters + dimes + nickels + pennies
 
-    // Print total number of coins to give the customer
-    printf("%i\n", coins);
-
-
+    # Print total number of coins to give the customer
+    print(coins)
 
 def get_cents():
     # Taking input from user
     while (True):
-        cents = get_float("Change owed: ")
+        cents = get_float("Change owed: ") * 100
         if (cents > 0):
             break
 
