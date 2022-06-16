fac9036c1f41d79afe4887e3489b57d34ce70825
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-16 16:09:44 +0000

    Thu, Jun 16, 2022, 4:09 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,6 +5,29 @@ def main():
     # Ask how many cents the customer is owed
     cents = get_cents()
 
+    # Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents)
+    cents = cents - quarters * 25
+
+    # Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
+
+    # Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents)
+    cents = cents - nickels * 5
+
+    # Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents)
+    cents = cents - pennies * 1
+
+    # Sum coins
+    int coins = quarters + dimes + nickels + pennies
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+
+
 
 def get_cents():
     # Taking input from user
@@ -12,3 +35,6 @@ def get_cents():
         cents = get_float("Change owed: ")
         if (cents > 0):
             break
+
+def calculate_quarters(cents):
+    
