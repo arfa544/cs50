71e164b7626d3cf607e21f1e5f0d96e83737e171
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-16 18:14:52 +0000

    Thu, Jun 16, 2022, 6:14 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -40,28 +40,27 @@ def calculate_quarters(cents):
     while (cents >= 25):
         cents = cents - 25
         quarters = quarters + 1
-        return quarters
+    return quarters
 
 def calculate_dimes(cents):
     dimes = 0
     while (cents >= 10):
         cents = cents - 10
         dimes = dimes + 1
-        return dimes
+    return dimes
 
 def calculate_nickels(cents):
     nickels = 0
     while (cents >= 5):
         cents = cents - 5
         nickels = nickels + 1
-        return nickels
+    return nickels
 
 def calculate_pennies(cents):
     pennies = 0
     while (cents >= 1):
         cents = cents - 1
         pennies = pennies + 1
-        print(pennies)
-        return pennies
+    return pennies
 
 main()
