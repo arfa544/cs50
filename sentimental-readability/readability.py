f79f9628f281e648a72418c59ccf157eea45902f
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-17 03:28:11 +0000

    Fri, Jun 17, 2022, 3:28 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -18,3 +18,15 @@ def main():
     L = letters / words * 100
     S = sentences / words * 100
     index = 0.0588 * L - 0.296 * S - 15.8
+
+    if (index > 16):
+        print("Grade 16+")
+    elif (index < 1):
+        print("Before Grade 1")
+    else:
+        print(f"Grade {round(index)}")
+
+
+# To get number of letters.
+def count_letters(text):
+    for i in 
