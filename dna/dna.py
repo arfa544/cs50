35120e2d62b2aee4149b8236a3e7cda32b8ed1c1
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-17 11:42:18 +0000

    Fri, Jun 17, 2022, 11:42 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -75,9 +75,8 @@ def longest_match(sequence, subsequence):
 
 def match(strs, dna_match, row):
     for str in strs:
-        if(dna_match[str] != row[str]):
+        if(dna_match[str] != int(row[str])):
             return False
-        #print(dna_match[str])
     return True
 
 
