a84c511d933312aa451e87c21b2edeeab346ca44
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-17 10:34:18 +0000

    Fri, Jun 17, 2022, 10:34 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -7,15 +7,15 @@ def main():
     # TODO: Check for command-line usage
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1], "r") as file:
-        reader = csv.DictReader(file)
+    with open(sys.argv[1], "r") as database_file:
+        database_reader = csv.DictReader(database_file)
 
     # TODO: Read DNA sequence file into a variable
-    with open(sys.argv[2], "r") as f:
-        DNA = f.read()
+    with open(sys.argv[2], "r") as dna_file:
+        DNA = dna_file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    
+
     # TODO: Check database for matching profiles
 
     return
