06cd54735623542202ffde12e4ab3639ca729e2f
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-17 06:52:41 +0000

    Fri, Jun 17, 2022, 6:52 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,7 +12,7 @@ def main():
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], "r") as f:
-        sequence = f.read()
+        DNA = f.read()
 
     # TODO: Find longest match of each STR in DNA sequence
     match = longest_match(sequence, subsequence)
