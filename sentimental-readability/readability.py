de206f49de965c0d26b2792d67bee2837c0b7b12
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-17 03:22:08 +0000

    Fri, Jun 17, 2022, 3:22 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,16 @@
 # TODO
+from cs50 import get_string
+
+
+def main():
+    # Taking input from user
+    text = get_string("Text: ")
+
+    # To get number of letters.
+    letters = count_letters(text)
+
+    # To get number of words.
+    words = count_words(text)
+
+    # To get number of sentencess.
+    sentences = count_sentences(text)
