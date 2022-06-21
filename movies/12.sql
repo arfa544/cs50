d690e5297e109ddfa018d8e5417e693b4dfe35ae
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-21 18:20:04 +0000

    Tue, Jun 21, 2022, 6:20 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,5 @@
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE name = "Johnny Depp" AND "Bonham Carter";
+WHERE name = "Johnny Depp"
+AND movie_id IN (SELECT movie_id FROM people JOIN stars.person_id = people.id WHERE name = "Bonham Carter");
