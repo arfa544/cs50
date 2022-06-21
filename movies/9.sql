e485ed534583138390d303e27c46ddf29c3b56e6
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-21 16:27:21 +0000

    Tue, Jun 21, 2022, 4:27 PM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,3 +1,3 @@
-SELECT count(distinct(name)) FROM people JOIN directors ON people.id = directors.person_id
+SELECT count((name)) FROM people JOIN directors ON people.id = directors.person_id
 JOIN ratings ON directors.movie_id = ratings.movie_id
 WHERE rating <= 9.0;
