312c986677dc620c3914baeb7cbf1198554eb42c
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-22 00:40:29 +0000

    Wed, Jun 22, 2022, 12:40 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,6 @@
 SELECT count(name) FROM people
-WHERE id in (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id
+JOIN stars ON stars.person_id = people.id
+WHERE name != "Kevin Bacon"
+AND movie_id IN
+(SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id
 WHERE name = "Kevin Bacon" AND birth = 1958);
