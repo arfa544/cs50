c83ee4f6fb7c29408fb6bedb0c2b1276896f55c0
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-22 14:35:50 +0000

    Wed, Jun 22, 2022, 2:35 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,4 @@
-SELECT name FROM people
+SELECT count(name) FROM people
 JOIN directors ON directors.person_id = people.id
 JOIN movies ON directors.movie_id = movies.id
 JOIN ratings ON directors.movie_id = ratings.movie_id
