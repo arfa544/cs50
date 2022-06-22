120a97e0118ade28cfb64e5d296c1a256ed9fa76
arfa <82232171+arfa544@users.noreply.github.com>
2022-06-22 00:32:55 +0000

    Wed, Jun 22, 2022, 12:32 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,2 +1,3 @@
 SELECT name FROM people
-WHERE id in (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id)
+WHERE id in (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id
+WHERE name = "Kevin Bacon" AND birth = 1958);
