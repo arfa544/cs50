a7336f86448a47d4dfc5b079b2359dd921b6fd61
arfa <82232171+arfa544@users.noreply.github.com>
2022-07-04 18:02:49 +0000

    Mon, Jul 4, 2022, 6:02 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -47,3 +47,5 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street"
 --3.As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call,
 --  I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
 --  The thief then asked the person on the other end of the phone to purchase the flight ticket.
+SELECT name from people
+JOIN 
