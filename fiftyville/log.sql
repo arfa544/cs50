6f9879c4d171e26fba2d19ccf9697d76c04e6e4e
arfa <82232171+arfa544@users.noreply.github.com>
2022-07-04 17:44:39 +0000

    Mon, Jul 4, 2022, 5:44 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,12 +6,14 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 --Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today
 --with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
 
-SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+SELECT transcript FROM interviews
+WHERE year = 2021 AND month = 7 AND day = 28 AND transcript like "%bakery%";
 
 --1.Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
 --  If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.
 SELECT name FROM people
-JOIN bakery_security_logs ON people.id = bakery_security_logs.id WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute = 15;
+JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute = 15;
 --No data found.
 
 --2.I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery,
