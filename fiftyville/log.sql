e755bcd7fd9670768da99ad9ababa90193840f1a
arfa <82232171+arfa544@users.noreply.github.com>
2022-07-05 11:50:49 +0000

    Tue, Jul 5, 2022, 11:50 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -51,5 +51,4 @@ SELECT name from people
 JOIN passengers ON people.passport_number = passengers.passport_number
 WHERE flight_id = (SELECT id FROM flights
 WHERE year = 2021 AND month = 7 AND day = 28
-ORDER BY hour,minute
-)
+ORDER BY hour,minute);
