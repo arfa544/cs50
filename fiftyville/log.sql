69304ec4429bc1fc9cf288f8571f6c4c7426102c
arfa <82232171+arfa544@users.noreply.github.com>
2022-07-05 12:04:55 +0000

    Tue, Jul 5, 2022, 12:04 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -14,7 +14,7 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND transcript like "%bakery%";
 SELECT name FROM people
 JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
 WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
---SUSPECTS
+--Suspects
 -- +---------+
 -- | Vanessa |
 -- | Bruce   |
@@ -32,7 +32,7 @@ SELECT name from people
 JOIN bank_accounts ON people.id = bank_accounts.person_id
 JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
 WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
---NEW SUSPECTS
+--New suspects
 -- +---------+
 -- | Bruce   |
 -- | Diana   |
@@ -52,6 +52,7 @@ JOIN passengers ON people.passport_number = passengers.passport_number
 WHERE flight_id = (SELECT id FROM flights
 WHERE year = 2021 AND month = 7 AND day = 29
 ORDER BY hour,minute LIMIT 1);
+--New suspects
 -- +--------+
 -- | Doris  |
 -- | Sofia  |
@@ -62,3 +63,6 @@ ORDER BY hour,minute LIMIT 1);
 -- | Kenny  |
 -- | Luca   |
 -- +--------+
+
+--Common suspects from above three conditions are
+--Bruce, Luca
