fdab199d2f83a5acb8a19921483b4fb18f9bf37f
arfa <82232171+arfa544@users.noreply.github.com>
2022-10-05 05:30:27 +0000

    Wed, Oct 5, 2022, 5:30 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,10 +19,33 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    
+    for (int i = 0; i < strlen(word); i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+    return score;
 }
