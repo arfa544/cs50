d988780326209520f2b3d70edb812867b60297b5
arfa <82232171+arfa544@users.noreply.github.com>
2022-10-30 10:07:48 +0000

    Sun, Oct 30, 2022, 10:07 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -113,12 +113,21 @@ def quote():
     """Get stock quote."""
     return apology("TODO")
 
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
-    return apology("TODO")
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
 
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+    else:
+        return render_template("register.html")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
