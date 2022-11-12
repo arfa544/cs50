01834c6adb3635d25ca7db3ee9d265b9e1d6c889
arfa <82232171+arfa544@users.noreply.github.com>
2022-11-12 13:08:45 +0000

    Sat, Nov 12, 2022, 1:08 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -50,8 +50,10 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
-
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("buy.html")
 
 @app.route("/history")
 @login_required
