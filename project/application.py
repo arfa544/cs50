8482112aeea5b90d755c4b0e4a82d2f6ab053153
arfa <82232171+arfa544@users.noreply.github.com>
2022-12-29 22:54:57 +0000

    Thu, Dec 29, 2022, 10:54 PM +00:00

--- project/application.py
+++ project/application.py
@@ -451,8 +451,9 @@ def register():
             if new_family_name is None:
                 flash("Please select a existing Family name, or provide a new family name.")
                 return redirect("/register")
+            db.execute("INSERT INTO family(family_name) VALUES (?)", new_family_name)
 
-            
+
         # creating new entry for user in family and profile table
         # db.execute("INSERT INTO profile (user_id, name, height, weight, bmi) VALUES (?, ?, ?, ?, ?)", user[0]["user_id"], username, 0, 0, 0)
         # db.execute("INSERT INTO profile (user_id, date, height, weight, bmi) VALUES (?, ?, ?, ?, ?)", user[0]["user_id"], datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 0, 0, 0)
