9a540c801819e786994026ad761e20bf09299c7a
arfa <82232171+arfa544@users.noreply.github.com>
2022-12-29 23:59:31 +0000

    Thu, Dec 29, 2022, 11:59 PM +00:00

--- project/application.py
+++ project/application.py
@@ -90,7 +90,7 @@ def logout():
 def index():
     rows = db.execute("SELECT * FROM users WHERE user_id = ?", session["user_id"])
 
-    history = db.execute("SELECT record_date, height, weight, bmi FROM profile WHERE user_id = ?", session["user_id"])
+    history = db.execute("SELECT record_date, height, weight, bmi FROM records WHERE user_id = ?", session["user_id"])
     for x in range(len(history)):
         if history[x]["bmi"] < 18.5:
             history[x].update(category = 'underweight')
