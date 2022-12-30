eca22e1c2a730a94a1cf3aad8e29d6c16c5ed1d0
arfa <82232171+arfa544@users.noreply.github.com>
2022-12-30 13:36:35 +0000

    Fri, Dec 30, 2022, 1:36 PM +00:00

--- project/application.py
+++ project/application.py
@@ -30,37 +30,6 @@ Session(app)
 db = SQL("sqlite:///project.db")
 
 
-@app.route('/bokeh')
-def bokeh():
-
-    # init a basic bar chart:
-    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
-    fig = figure(frame_width=600, frame_height=600)
-    fig.vbar(
-        x=[1, 2, 3, 4],
-        width=0.5,
-        bottom=0,
-        top=[1.7, 2.2, 4.6, 3.9],
-        color='navy'
-    )
-
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
-
-    # render template
-    script, div = components(fig)
-    html = render_template(
-        'bokeh.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    # return util.string.encode_utf8(html)
-    return html
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
