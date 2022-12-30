72a8a3c5d0aec0553314fb07389eeeb77e14bb88
arfa <82232171+arfa544@users.noreply.github.com>
2022-12-30 00:25:21 +0000

    Fri, Dec 30, 2022, 12:25 AM +00:00

--- project/application.py
+++ project/application.py
@@ -372,7 +372,7 @@ def profile():
             # db.execute("UPDATE family SET weight= ?, height = ?, bmi = ? WHERE user_id = ? and name = ?", kgs, cms, bmi, session["user_id"], user[0]["username"])
 
             # Insert into records
-            db.execute("INSERT INTO records(user_id, height, weight, bmi, record_date) VALUES(?,?,?,?,?)", session["user_id"], cms, kgs, bmi, )
+            db.execute("INSERT INTO records(user_id, height, weight, bmi, record_date) VALUES(?,?,?,?,?)", session["user_id"], cms, kgs, bmi, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
         # metric units were chosen
         elif 'updateMe' in request.form:
