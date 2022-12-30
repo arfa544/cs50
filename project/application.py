0252e205c9499be637a038d18cb11560d30c5ca2
arfa <82232171+arfa544@users.noreply.github.com>
2022-12-30 11:53:22 +0000

    Fri, Dec 30, 2022, 11:53 AM +00:00

--- project/application.py
+++ project/application.py
@@ -10,6 +10,46 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import login_required
 
+
+# TEST BOKEH
+
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+
+@app.route('/bokeh')
+def bokeh():
+
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    fig = figure(plot_width=600, plot_height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
+
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'index.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return encode_utf8(html)
+    
+## END OF BOKEH TEST
+
+
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
