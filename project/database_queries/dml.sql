508b2033ae192c06200e3c519ca28d0dbb0577b9
arfa <82232171+arfa544@users.noreply.github.com>
2022-12-30 23:52:55 +0000

    Fri, Dec 30, 2022, 11:52 PM +00:00

--- project/database_queries/dml.sql
+++ project/database_queries/dml.sql
@@ -1,9 +1,9 @@
 -- Insert values in users table
 INSERT INTO users (user_id, user_name, password, email) VALUES
-(1,'Arfa',12,'arfa@gmail.com'),
-(2,'Ataa',12,'arfa@gmail.com'),
-(3,'Arfa',12,'arfa@gmail.com'),
-(4,'Arfa',12,'arfa@gmail.com'),
+(1,'Kathryn Castillo',12,'Kathryn@gmail.com'),
+(2,'Kai Alexander',12,'Kai@gmail.com'),
+(3,'Lyla Fleming',12,'Lyla@gmail.com'),
+(4,'Fernando Barrera',12,'arfa@gmail.com'),
 (5,'Arfa',12,'arfa@gmail.com'),
 (6,'Arfa',12,'arfa@gmail.com'),
 (7,'Arfa',12,'arfa@gmail.com'),
