acb014bf85303ac0d7c4cbffbb1c3d46119fdb70
arfa <82232171+arfa544@users.noreply.github.com>
2022-12-30 11:59:51 +0000

    Fri, Dec 30, 2022, 11:59 AM +00:00

--- project/application.py
+++ project/application.py
@@ -16,7 +16,7 @@ from helpers import login_required
 from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
+from bokeh.util.strings import encode_utf8
 
 @app.route('/bokeh')
 def bokeh():
